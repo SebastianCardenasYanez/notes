1f674b61379563d1f4fd23c042cb7c2afd546f45
SebastianCardenasYanez <160754840+SebastianCardenasYanez@users.noreply.github.com>
2024-10-29 07:27:06 -0500

    feat: :sparkles: vite base config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://sebastiancardenasyanez.github.io/notes/",
   server: {
     port: 8080,
   }
